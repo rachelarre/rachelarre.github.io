5120a31f65d34457d729d05b7439f01ce5d5fa22
Rachel Arre <bluepebbl@gmail.com>
2020-10-30 14:22:50 -0400

    Adding Image Toggle Functionality

--- PUI2020/homework_6/images.js
+++ PUI2020/homework_6/images.js
@@ -1,17 +1,30 @@
-const couchImages = {
-  cd: "assets/colors/CozyDenim.png",
-  mh: "assets/colors/MorningHaze.png",
-  as: "assets/colors/AfterSchoolSpecial.png",
-  rd: "assets/colors/RainyDay.png",
+const productImages = {
+  cd: "assets/product_gallery/couch-rainy.png",
+  mh: "assets/product_gallery/couch-cozy.png",
+  as: "assets/product_gallery/floor-after.png",
+  rd: "assets/product_gallery/round-morning.png",
 };
 
 changeImage();
 function changeImage() {
   let colorSelect = document.querySelectorAll(".colorToggle");
+  let pillowColors = {
+    cd: "Cozy Denim",
+    mh: "Morning Haze",
+    as: "After School Special",
+    rd: "Rainy Day",
+  };
 
   colorSelect.forEach((element) => {
     element.addEventListener("click", () => {
       console.log("ColorCLick");
+      const imageid = element.id;
+      if (productImages[imageid]) {
+        document.getElementById("product-image-lg").src =
+          productImages[imageid];
+        document.getElementById("pillowColor").innerText =
+          pillowColors[imageid];
+      }
     });
   });
 }
